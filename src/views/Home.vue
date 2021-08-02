2ef5e58c7c95771fa5a7ccdfaf101c4b3182b349
Donutz <do12nut06@gmail.com>
2021-08-02 20:00:26 +0700

    Add Delete complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Welcome to my to-do app</h1>
+  <div class="home-page">
+    <h1>Home Page</h1>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.home-page {
+  width: 100%;
+  display: flex;
+  padding-top: 160px;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,3 +1,47 @@
 <template>
-  <h1>Todo page</h1>
+  <div class="todo-page">
+    <h1>Todo page</h1>
+    <form @submit.prevent="handleSubmit">
+      <input v-model.lazy="newTodo" placeholder="Type something..." />
+      <button type="submit">Submit</button>
+    </form>
+    <TodoList :todos="todos" />
+  </div>
 </template>
+
+<script>
+import TodoList from "../components/TodoList.vue";
+
+export default {
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+      nextTodoId: 0,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      if (!this.newTodo) return;
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodo,
+      });
+      this.newTodo = "";
+    },
+  },
+};
+</script>
+
+<style>
+.todo-page {
+  width: 100%;
+  display: flex;
+  padding-top: 160px;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
